84b925eb63189f3a19740f29859ea37eb1e0266b
Colin Haley <admin@colinhaley.net>
2015-12-08 12:55:10 -0500

    Updates

--- Scripts/CentOSProfileSetup.sh
+++ Scripts/CentOSProfileSetup.sh
@@ -23,9 +23,24 @@ EOF
 # Install git
 yum -y install git
 
+# Git Configs
+git config credential.helper store
+git config --global credential.helper 'cache --timeout 7200'
+
+
 # Install Python
 yum -y install Python
 
 # Enable EPEL Repo: http://www.tecmint.com/how-to-enable-epel-repository-for-rhel-centos-6-5/
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
+
+# Install Ansible
+yum -y install ansible
+
+# Install Vagrant
+mkdir ~/temp
+cd ~/temp
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.2_x86_64.rpm -O vagrant_1.4.2_x86_64.rpm
+sudo rpm -i vagrant_1.4.2_x86_64.rpm
+rm -rf ~/temp
